306458a8d811c2c06b4b39b65eaffdaad32d7b6c
Jerry123i <igorsc.jerry@gmail.com>
2018-03-23 12:23:56 -0300

    TileAgregator

---
+++ Assets/Editor/TileAgregatorEditor.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ColliderAgregator))]
+public class TileAgregatorEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        var obj = target as ColliderAgregator;
+
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Full Agregate", GUILayout.Height(35)))
+        {
+            obj.myCollider = obj.gameObject.GetComponent<BoxCollider2D>();
+            GetChildrensBox(obj);
+            CenterOnChildren(obj);
+            AddCollidersList(obj.myCollider, obj.childColiders);
+
+            foreach (BoxCollider2D child in obj.childColiders)
+            {
+
+                if (child.gameObject.GetInstanceID() != obj.gameObject.GetInstanceID())
+                {
+                    child.enabled = false;
+                }
+
+            }
+
+        }
+
+        if (GUILayout.Button("Get"))
+        {
+            GetChildrensBox(obj);
+        }
+
+        if (GUILayout.Button("Center"))
+        {
+            CenterOnChildren(obj);
+        }
+
+        if (GUILayout.Button("AgregateLayers"))
+        {
+            AddCollidersList(obj.myCollider, obj.childColiders);
+        }
+
+        if(GUILayout.Button("Toggle Children"))
+        {
+            foreach(BoxCollider2D child in obj.childColiders)
+            {
+
+                if(child.gameObject.GetInstanceID() !=  obj.gameObject.GetInstanceID())
+                {
+                    child.enabled = !child.enabled;
+                }
+                
+            }
+
+        }
+
+        
+
+    }
+    
+    void CenterOnChildren(ColliderAgregator obj)
+    {
+        List<Vector3> childrenPlaces =  new List<Vector3>();
+
+        foreach (BoxCollider2D child in obj.childColiders)
+        {
+            if (child.gameObject.GetInstanceID() != obj.gameObject.GetInstanceID())
+            {
+                childrenPlaces.Add(child.gameObject.transform.position);
+            }
+        }
+
+        obj.transform.position = (GetAvarageFromList(childrenPlaces));
+
+        for(int i = 0; i<childrenPlaces.Count; i++)
+        {
+            obj.childColiders[i + 1].transform.position = childrenPlaces[i];
+        }
+
+    }
+
+    Vector3 GetAvarageFromList(List<Vector3> l)
+    {
+        Vector3 v;
+        v = new Vector3(0, 0, 0);
+
+        for(int i = 0; i < l.Count; i++)
+        {
+            v += l[i];
+        }
+
+        return (v / l.Count);
+    }
+
+
+    public void AddCollidersList(BoxCollider2D target, BoxCollider2D[] lista)
+    {
+
+        if (lista.Length == 1)
+        {
+            Debug.Log("Collider Agregator error. List too short");
+        }
+
+        target.size = new Vector2(lista[1].size.x, lista[1].size.y);
+        target.offset = new Vector2(lista[1].offset.x, lista[1].offset.y);
+
+        for (int i = 2; i < lista.Length; i++)
+        {
+            AddColliders(target, target, lista[i]);
+        }
+
+    }
+
+    public void AddColliders(BoxCollider2D target, BoxCollider2D a, BoxCollider2D b)
+    {
+
+        target.size = new Vector2((a.size.x + b.size.x), (b.size.y));
+
+        if (a.offset == b.offset)
+        {
+            // target.offset = new Vector2(a.offset.x, a.offset.y);
+        }
+
+        target.offset = new Vector2((a.offset.x + b.offset.x) / 2, ((a.offset.y + b.offset.y) / 2));
+    }
+
+    void GetChildrensBox(ColliderAgregator t)
+    {
+        t.childColiders = t.gameObject.transform.GetComponentsInChildren<BoxCollider2D>();
+
+    }
+
+}
---
+++ Assets/Scripts/ColliderAgregator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class ColliderAgregator : MonoBehaviour {
+
+    
+    public BoxCollider2D myCollider;
+    public BoxCollider2D[] childColiders;
+
+    private void Awake()
+    {
+        //myCollider = this.GetComponent<BoxCollider2D>();
+        //childColiders = transform.GetComponentsInChildren<BoxCollider2D>();
+        //AddCollidersList(myCollider, childColiders);
+
+                
+    }
+    
+    public void AddColliders(BoxCollider2D target,BoxCollider2D a, BoxCollider2D b)
+    {
+        
+        target.size = new Vector2((a.size.x + b.size.x), (b.size.y));
+
+        if(a.offset == b.offset)
+        {
+           // target.offset = new Vector2(a.offset.x, a.offset.y);
+        }
+
+        target.offset =  new Vector2 ((a.offset.x + b.offset.x) / 2, ((a.offset.y + b.offset.y) / 2));
+
+                
+    }
+
+    public void AddCollidersList(BoxCollider2D target, BoxCollider2D[] lista)
+    {
+        
+        if (lista.Length == 0)
+        {
+            Debug.Log("Collider Agregator error. List too short");            
+        }
+
+        target.size = new Vector2(lista[0].size.x, lista[0].size.y);
+        target.offset = new Vector2(lista[0].offset.x, lista[0].offset.y);
+
+        for (int i = 1; i<lista.Length; i++)
+        {
+            AddColliders(target, target, lista[i]);
+        }
+
+    }
+
+}
