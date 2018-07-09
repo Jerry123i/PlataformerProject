b9e738c7050b3d40ac7f46d6dc611110f87a638f
Igor <igorsc.jerry@gmail.com>
2018-07-09 12:15:21 -0300

    LaserSpawning

--- Assets/LaserScript.cs
+++ Assets/LaserScript.cs
@@ -4,13 +4,69 @@ using UnityEngine;
 
 public class LaserScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public GameObject laserPrefab;
+
+    private Animator animator;
+
+    public int lenghtOfLaser;
+
+    public float downTime;
+    public float upTime;
+    public float startDelay;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(FireRoutine(startDelay, upTime));
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    IEnumerator FireRoutine(float dowT, float upT)
+    {
+        yield return new WaitForSeconds(dowT);
+
+        StartShooting();
+
+        yield return new WaitForSeconds(upT);
+        StopShooting();
+        StartCoroutine(FireRoutine(downTime, upTime));
+
+    }
+
+    void StartShooting()
+    {
+        GameObject ray = null;
+
+        animator.SetTrigger("Fire");
+
+        for(int i = 0; i<lenghtOfLaser; i++)
+        {
+            if (i == 0)
+            {
+                ray = Instantiate(laserPrefab, transform);
+                ray.transform.Translate(0.0f, 0.0f, 0.0f, transform);
+            }
+            else
+            {
+                GameObject rayB = Instantiate(laserPrefab, transform);
+                rayB.transform.Translate((2.5f * i), 0.0f, 0.0f, ray.transform);
+                ray = rayB;
+            }
+        }
+
+    }
+
+    void StopShooting()
+    {
+        animator.SetTrigger("Stop");
+    }
+
 }
