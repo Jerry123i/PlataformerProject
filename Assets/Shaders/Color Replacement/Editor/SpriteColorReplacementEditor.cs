98dabfccf70bdccb51dd28fa6e1bfba66c518dc2
Igor <igorsc.jerry@gmail.com>
2018-10-19 17:12:36 -0300

    GrayShader

---
+++ Assets/Shaders/Color Replacement/Editor/SpriteColorReplacementEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SpriteColorReplacement))]
+public class SpriteColorReplacementEditor : Editor 
+{
+	public override void OnInspectorGUI ()
+	{
+		EditorGUILayout.HelpBox ("Lembrando que aguenta 10 cores.", MessageType.Warning);
+		DrawDefaultInspector();
+
+		var scr = (SpriteColorReplacement)target;
+
+		if (GUILayout.Button ("Apply")) 
+		{
+			scr.Apply ();
+			Debug.Log ("fuck");
+		}
+	}
+}
---
+++ Assets/Shaders/Color Replacement/SpriteColorReplacement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteColorReplacement : MonoBehaviour 
+{
+	[Serializable]
+	public struct ReplacedColor
+	{
+		public Color original;
+		public Color replaced;
+	}
+
+	public List<ReplacedColor> replacedColors = new List<ReplacedColor>();
+
+    private Material mat;
+
+	public void Awake()
+	{
+		mat.SetInt("_ArrayLenght", replacedColors.Count);
+        mat.SetColorArray("_RepColor", new Color[10]);
+        mat.SetColorArray("_OriColor", new Color[10]);
+
+		Apply ();
+	}
+
+	public void Apply()
+	{
+		if (!mat) mat = GetComponent<Renderer>().material;
+
+		var originalColorArray = 
+			from col in replacedColors
+			select col.original;
+
+		var replacedColorArray = 
+			from col in replacedColors
+			select col.replaced;
+
+		mat.SetInt("_ArrayLenght", replacedColors.Count);
+		mat.SetColorArray("_RepColor", replacedColorArray.ToArray());
+        mat.SetColorArray("_OriColor", originalColorArray.ToArray());
+    }
+}
